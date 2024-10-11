95eabfe12e2e9c4e3ddb916cbda083c002a37e34
BlueDragon320 <vaibhav.b.g.patil@gmail.com>
2024-10-11 23:39:04 +0530

    Python Programming

---
+++ input.py
@@ -0,0 +1,11 @@
+# input [it is used to take input from user]
+# name = [assign vaiable]
+"""
+This is a comment
+"""
+
+name = input("What's your name? ")
+print("Hello," + name)
+print("Hello, ", name)
+
+#To use multiple vairables we can use ' , ' instead of +
