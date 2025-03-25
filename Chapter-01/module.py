a0d99ebb421ef41dda6a8c0d46ef2c4ce483dc76
bluedragon320 <vaibhav.b.g.patil@gmail.com>
2025-03-25 23:55:18 +0530

    Completed Chapter 01

---
+++ Chapter-01/module.py
@@ -0,0 +1,5 @@
+import pyjokes
+# print ("Printing jokes...")
+# This prints a random jokes
+joke = pyjokes.get_joke()
+print(joke)
---
+++ PS CH-01/p2.py
@@ -0,0 +1,2 @@
+# Use REPL and print the table of 5 using it.
+# Terminal > type python Enter > calculate
